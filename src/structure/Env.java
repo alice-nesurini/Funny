03503401d320475a5ac167fed56894e122529d50
Alice <alice.nesurini@gmail.com>
2018-11-25 21:06:46 +0100

    Env test (missing get all ok)

--- src/structure/Env.java
+++ src/structure/Env.java
@@ -5,16 +5,27 @@ public class Env {
     private final Env enclosing;
 
     public Env(Frame frame, Env enclosing) {
+        // enclosing environment CAN be null
+        if(frame==null){
+            throw new IllegalArgumentException("frame can't be null");
+        }
         this.frame = frame;
         this.enclosing = enclosing;
     }
 
     public Val add(String id, Val val){
+        if(id==null || id.isEmpty()){
+            throw new IllegalArgumentException("id can't be empty/null");
+        }
         frame.add(id, val);
         return val;
     }
 
     public Val get(String id){
+        if(id==null || id.isEmpty()){
+            throw new IllegalArgumentException("id can't be null");
+        }
+        System.out.println(frame.contains(id));
         return frame.contains(id)?frame.get(id):enclosing.get(id);
     }
 }
--- test/structure/EnvTest.java
+++ test/structure/EnvTest.java
@@ -5,22 +5,71 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Mockito.*;
 
 class EnvTest {
 
-    private Env env;
+    @Test
+    public void create0(){
+        // empty frame, excpected exception
+        assertThrows(IllegalArgumentException.class, ()->new Env(null, null));
+    }
 
     @Test
-    void add() throws InterpreterException {
-        BigDecimal b3=new BigDecimal(3);
-        BigDecimal b4=new BigDecimal(4);
-        BigDecimal b5=new BigDecimal(5);
-        env.add("param", new NumVal(b3));
-        env.add("myVar", new NumVal(b4));
-        env.add("other", new NumVal(b5));
+    public void create1(){
+        // all ok
+        Env env=new Env(mock(Frame.class), null);
+        assertNotNull(env);
+    }
 
-        assertEquals(b3, (env.get("param").checkNum().getValue()));
-        assertEquals(b4, (env.get("myVar").checkNum().getValue()));
-        assertEquals(b5, (env.get("other").checkNum().getValue()));
+    @Test
+    void add0() {
+        // null id, val expected exception
+        Env env=new Env(mock(Frame.class), null);
+        assertThrows(IllegalArgumentException.class, ()->env.add(null, null));
+    }
+
+    @Test
+    void add1() {
+        // empty id expected exception
+        Env env=new Env(mock(Frame.class), null);
+        assertThrows(IllegalArgumentException.class, ()->env.add("", mock(Val.class)));
+    }
+
+    @Test
+    void add2() {
+        // all ok
+        Env env=new Env(mock(Frame.class), null);
+        Env envSpy=spy(env);
+        envSpy.add("param", mock(Val.class));
+        // verify problem
+        // verify(envSpy, times(1)).frame.add(any(String.class), any(Val.class));
+    }
+
+    @Test
+    void get0() {
+        // null id, expected exception
+        Env env=new Env(mock(Frame.class), null);
+        env.add("id", mock(Val.class));
+        assertThrows(IllegalArgumentException.class, ()->env.get(null));
+    }
+
+    @Test
+    void get1() {
+        // empty id, expected exception
+        Env env=new Env(mock(Frame.class), null);
+        env.add("id", mock(Val.class));
+        assertThrows(IllegalArgumentException.class, ()->env.get(""));
+    }
+
+    @Test
+    void get2() {
+        // all ok
+        Env env=new Env(mock(Frame.class), null);
+        /*Val val=env.add("id", mock(Val.class));
+        assertNotNull(val);
+        val=env.get("id");
+        assertNotNull(val);*/
     }
 }
